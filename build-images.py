aab44f883dcbb85c41e15b0403376d3ed57655c5
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-04-15 12:55:34 +0800

    some preliminary containers that can run cryptography's tests (#5)
    * some preliminary containers that can run cryptography's tests
    
    * add libressl and stretch, remove sid, various updates
    
    * also python3
    
    * review feedback
    
    * more review feedback and a bit more output from the shell script
    
    * builders -> runners
    
    * more review feedback, update python build script to do the new images
    
    * one rm and an empty list to avoid a None guard

--- build-images.py
+++ build-images.py
@@ -1,22 +1,59 @@
 #!/usr/bin/env python
 
+import collections
+import os
 import subprocess
 
 
+Image = collections.namedtuple("Image", ["tag", "path", "build_args"])
+
 IMAGES = [
-    ("jenkins", "crypto-jenkins"),
-    ("caddy", "caddy"),
+    Image(
+        tag="crypto-jenkins", path=["jenkins"], build_args=[]
+    ),
+    Image(
+        tag="caddy", path=["caddy"], build_args=[]
+    ),
+    Image(
+        tag="cryptography-runner:centos7",
+        path=["runners", "centos7"],
+        build_args=[]
+    ),
+    Image(
+        tag="cryptography-runner:jessie",
+        path=["runners", "jessie"],
+        build_args=[]
+    ),
+    Image(
+        tag="cryptography-runner:stretch",
+        path=["runners", "stretch"],
+        build_args=[]
+    ),
+    Image(
+        tag="cryptography-runner:jessie-libressl-2.4.5",
+        path=["runners", "jessie-libressl"],
+        build_args=["LIBRE_VERSION=2.4.5"],
+    ),
+    Image(
+        tag="cryptography-runner:jessie-libressl-2.5.3",
+        path=["runners", "jessie-libressl"],
+        build_args=["LIBRE_VERSION=2.5.3"],
+    ),
 ]
 
 
-def docker_build(path, tag):
-    subprocess.check_call(["docker", "build", "-t", tag, path])
+def docker_build(image):
+    shell_cmd = ["docker", "build", "-t", image.tag, os.path.join(*image.path)]
+    for build_arg in image.build_args:
+        shell_cmd += ["--build-arg", build_arg]
+
+    subprocess.check_call(shell_cmd)
 
 
 def main():
-    for path, tag_name in IMAGES:
-        print("=== Building {}".format(path))
-        docker_build(path, tag=tag_name)
+    for image in IMAGES:
+        print("=== Building {}".format(image.tag))
+        docker_build(image)
 
 
 if __name__ == "__main__":
