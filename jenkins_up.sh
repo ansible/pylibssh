234c664149a66fcebba6ebe62eb6d4226ead43e1
Alex Gaynor <alex.gaynor@gmail.com>
2017-04-14 21:28:04 -0400

    Fixed #7 -- unit files for running caddy and the jenkins server (#8)
    * Fixed #7 -- unit files for running caddy and the jenkins server
    
    * Depend on jenkins

--- jenkins_up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home --name jenkins crypto-jenkins
-# The mount to /root/.caddy is just to cache certs
-docker run -d -p 443:443 -p 80:80 --link jenkins:jenkins -v /caddy:/root/.caddy --name caddy caddy
