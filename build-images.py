cff17a871c90df71ecd50b5f914a424244ada2b5
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-04-26 06:38:54 -0500

    modify build-images to use a json file to share with a jenkinsfile (#55)
    * modify build-images to use a json file to share with a jenkinsfile
    
    * also build jenkins and caddy

--- build-images.py
+++ build-images.py
@@ -1,59 +1,28 @@
 #!/usr/bin/env python
 
 import collections
+import json
 import os
 import subprocess
 
 
 Image = collections.namedtuple("Image", ["tag", "path", "build_args"])
 
-IMAGES = [
-    Image(
-        tag="crypto-jenkins", path=["jenkins"], build_args=[]
-    ),
-    Image(
-        tag="caddy", path=["caddy"], build_args=[]
-    ),
-    Image(
-        tag="cryptography-runner-centos7:latest",
-        path=["runners", "centos7"],
-        build_args=[]
-    ),
-    Image(
-        tag="cryptography-runner-wheezy:latest",
-        path=["runners", "wheezy"],
-        build_args=[],
-    ),
-    Image(
-        tag="cryptography-runner-jessie:latest",
-        path=["runners", "jessie"],
-        build_args=[]
-    ),
-    Image(
-        tag="cryptography-runner-stretch:latest",
-        path=["runners", "stretch"],
-        build_args=[]
-    ),
-    Image(
-        tag="cryptography-runner-sid:latest",
-        path=["runners", "sid"],
-        build_args=[]
-    ),
-    Image(
-        tag="cryptography-runner-jessie-libressl:2.4.5",
-        path=["runners", "jessie-libressl"],
-        build_args=["LIBRE_VERSION=2.4.5"],
-    ),
-    Image(
-        tag="cryptography-runner-jessie-libressl:2.5.3",
-        path=["runners", "jessie-libressl"],
-        build_args=["LIBRE_VERSION=2.5.3"],
-    ),
-]
+IMAGES = []
+
+with open("config.json") as config:
+    for conf in json.load(config):
+        IMAGES.append(
+            Image(
+                tag=conf['tag'],
+                path=conf['path'],
+                build_args=conf['build_args']
+            )
+        )
 
 
 def docker_build(image):
-    full_path = os.path.join(os.path.dirname(__file__), *image.path)
+    full_path = os.path.join(os.path.dirname(__file__), image.path)
     shell_cmd = ["docker", "build", "-t", image.tag, full_path]
     for build_arg in image.build_args:
         shell_cmd += ["--build-arg", build_arg]
