690ebe26324d7f956f88f5bd4a6e14cc37753f72
Paul Kehrer <paul.l.kehrer@gmail.com>
2019-05-25 12:01:14 -0400

    Openssl artifact (#218)
    * package the openssl artifacts in a scratch container
    
    * we're just going to build this separately.
    
    * fix yaml issue
    
    * nanoserver versions are the worst
    
    * test
    
    * try this
    
    * this might work

--- windows/openssl/build_openssl.bat
+++ windows/openssl/build_openssl.bat
@@ -6,13 +6,13 @@ echo %VSVERSION%
 cd openssl-1*
 
 if "%VSVERSION%" == "2010" (
-    if "%BUILDARCH%" == "x86" (
+    if "%BUILDARCH%" == "win32" (
         CALL C:\scripts\build_openssl_win32_2010.bat
     ) else (
         CALL C:\scripts\build_openssl_win64_2010.bat
     )
 ) else (
-    if "%BUILDARCH%" == "x86" (
+    if "%BUILDARCH%" == "win32" (
         CALL C:\scripts\build_openssl_win32_2015.bat
     ) else (
         CALL C:\scripts\build_openssl_win64_2015.bat
