5e7fa5a5777e310fb587391f935f4afd8e3dbfc1
Paul Kehrer <paul.l.kehrer@gmail.com>
2019-09-12 19:40:31 +0800

    python 3.7.4 on windows (#247)

--- windows/py3/scripts/install_everything.ps1
+++ windows/py3/scripts/install_everything.ps1
@@ -14,5 +14,5 @@ if ($LastExitCode -ne 0) { exit 1; }
 & C:/scripts/install_python.ps1 3.6.8 Python36 $env:CPU_ARCH
 if ($LastExitCode -ne 0) { exit 1; }
 
-& C:/scripts/install_python.ps1 3.7.3 Python37 $env:CPU_ARCH
+& C:/scripts/install_python.ps1 3.7.4 Python37 $env:CPU_ARCH
 if ($LastExitCode -ne 0) { exit 1; };
