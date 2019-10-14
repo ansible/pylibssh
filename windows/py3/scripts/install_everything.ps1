45dc232144561fcf4fd3f2e5085f2183945e64ad
Alex Gaynor <alex.gaynor@gmail.com>
2019-10-14 19:14:58 -0400

    Install python3.8 into the windows py3 container (#248)

--- windows/py3/scripts/install_everything.ps1
+++ windows/py3/scripts/install_everything.ps1
@@ -16,3 +16,6 @@ if ($LastExitCode -ne 0) { exit 1; }
 
 & C:/scripts/install_python.ps1 3.7.4 Python37 $env:CPU_ARCH
 if ($LastExitCode -ne 0) { exit 1; };
+
+& C:/scripts/install_python.ps1 3.8.0 Python38 $env:CPU_ARCH
+if ($LastExitCode -ne 0) { exit 1; };
