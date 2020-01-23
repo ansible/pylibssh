dc3520715d6233cf499543003c32d7e84ff88401
Ganesh B Nalawade <ganesh634@gmail.com>
2020-01-23 12:19:32 +0530

    Update setup.py to dynamically fetch sources list

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 import os
 import sys
 
+from glob import glob
 from setuptools import setup
 from setuptools.extension import Extension
 
@@ -12,23 +13,12 @@ from Cython.Build import cythonize
 
 LIB_NAME = 'ssh'
 
-
 sys.path.insert(0, os.path.abspath('lib'))
 
+sources = glob('lib/pylibssh/*.pyx')
+names = ['.'.join(src.replace(os.path.sep, '.').split('.')[1:-1]) for src in sources]
+extensions = [Extension(names[i], [sources[i]], libraries=[LIB_NAME]) for i in range(len(sources))]
 
 __name__ == '__main__' and setup(  # noqa: WPS428
-    ext_modules=cythonize([
-        Extension(
-            'pylibssh.session', ['lib/pylibssh/session.pyx'], libraries=[LIB_NAME],
-        ),
-        Extension(
-            'pylibssh.channel', ['lib/pylibssh/channel.pyx'], libraries=[LIB_NAME],
-        ),
-        Extension(
-            'pylibssh.sftp', ['lib/pylibssh/sftp.pyx'], libraries=[LIB_NAME],
-        ),
-        Extension(
-            'pylibssh.errors', ['lib/pylibssh/errors.pyx'], libraries=[LIB_NAME],
-        ),
-    ]),
+    ext_modules=cythonize(extensions)
 )
