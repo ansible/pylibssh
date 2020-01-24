e00da5660c61f1012b29933c1726b16095d4fed6
Ganesh B Nalawade <ganesh634@gmail.com>
2020-01-24 20:04:18 +0530

    Update sftp error handling
    *  Add support to fetch sftp error message

--- src/pylibsshext/sftp.pyx
+++ src/pylibsshext/sftp.pyx
@@ -22,12 +22,28 @@ from pylibsshext.session cimport get_libssh_session
 from pylibsshext.errors cimport LibsshSFTPException
 
 
+MSG_MAP = {
+    sftp.SSH_FX_OK: "No error",
+    sftp.SSH_FX_EOF: "End-of-file encountered",
+    sftp.SSH_FX_NO_SUCH_FILE: "File doesn't exist",
+    sftp.SSH_FX_PERMISSION_DENIED: "Permission denied",
+    sftp.SSH_FX_FAILURE: "Generic failure",
+    sftp.SSH_FX_BAD_MESSAGE: "Garbage received from server",
+    sftp.SSH_FX_NO_CONNECTION: "No connection has been set up",
+    sftp.SSH_FX_CONNECTION_LOST: "There was a connection, but we lost it",
+    sftp.SSH_FX_OP_UNSUPPORTED: "Operation not supported by the server",
+    sftp.SSH_FX_INVALID_HANDLE: "Invalid file handle",
+    sftp.SSH_FX_NO_SUCH_PATH: "No such file or directory path exists",
+    sftp.SSH_FX_FILE_ALREADY_EXISTS: "An attempt to create an already existing file or directory has been made",
+    sftp.SSH_FX_WRITE_PROTECT: "We are trying to write on a write-protected filesystem",
+    sftp.SSH_FX_NO_MEDIA: "No media in remote drive"
+}
 cdef class SFTP:
     def __cinit__(self, session):
         self.session = session
         self._libssh_sftp_session = sftp.sftp_new(get_libssh_session(session))
         if self._libssh_sftp_session is NULL:
-            raise LibsshSFTPException("Failed to create new session", session)
+            raise LibsshSFTPException("Failed to create new session")
         if sftp.sftp_init(self._libssh_sftp_session) != libssh.SSH_OK:
             raise LibsshSFTPException("Error initializing SFTP session")
 
@@ -42,16 +58,18 @@ cdef class SFTP:
             remote_file_b = remote_file
             if isinstance(remote_file_b, unicode):
                 remote_file_b = remote_file.encode("utf-8")
+
             rf = sftp.sftp_open(self._libssh_sftp_session, remote_file_b, O_WRONLY | O_CREAT | O_TRUNC, sftp.S_IRWXU)
             if rf is NULL:
-                raise LibsshSFTPException(self, "Opening remote file [%s] for write failed" % remote_file)
+                raise LibsshSFTPException("Opening remote file [%s] for write failed with error [%s]" % (remote_file, MSG_MAP.get(self._get_sftp_error_str())))
             buffer = f.read(1024)
+
             while buffer != b"":
                 length = len(buffer)
                 written = sftp.sftp_write(rf, PyBytes_AS_STRING(buffer), length)
                 if written != length:
                     sftp.sftp_close(rf)
-                    raise LibsshSFTPException(self, "Writing to remote file [%s] failed" % remote_file)
+                    raise LibsshSFTPException("Writing to remote file [%s] failed" % remote_file)
                 buffer = f.read(1024)
             sftp.sftp_close(rf)
 
@@ -65,7 +83,7 @@ cdef class SFTP:
 
         rf = sftp.sftp_open(self._libssh_sftp_session, remote_file_b, O_RDONLY, sftp.S_IRWXU)
         if rf is NULL:
-            raise LibsshSFTPException("Opening remote file [%s] for read failed" % remote_file)
+            raise LibsshSFTPException("Opening remote file [%s] for read failed with error [%s]" % (remote_file, MSG_MAP.get(self._get_sftp_error_str())))
 
         while True:
             file_data = sftp.sftp_read(rf, <void *>read_buffer, sizeof(char) * 1024)
@@ -81,3 +99,6 @@ cdef class SFTP:
                     sftp.sftp_close(rf)
                     raise LibsshSFTPException("Opening remote file [%s] for read failed" % remote_file)
         sftp.sftp_close(rf)
+
+    def _get_sftp_error_str(self):
+        return sftp.sftp_get_error(self._libssh_sftp_session)
