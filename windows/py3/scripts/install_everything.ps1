d1869e275fafef2bf4370d315e47b0f2b3ae2a30
Alex Gaynor <alex.gaynor@gmail.com>
2020-02-28 10:06:25 -0500

    Delete py34 and try to get Windows builds working (#257)

--- windows/py3/scripts/install_everything.ps1
+++ windows/py3/scripts/install_everything.ps1
@@ -1,16 +1,6 @@
 & C:/scripts/vs.bat
 if ($LastExitCode -ne 0) { exit 1; };
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-choco install --no-progress -y 7zip
-# We need to check exit code after every exe invocation because powershell has no
-# facility for doing this since Windows exit codes are inconsistent
-if ($LastExitCode -ne 0) { exit 1; }
-
-choco install --no-progress -y git --params="/NoAutoCrlf"
-if ($LastExitCode -ne 0) { exit 1; }
-
 & C:/scripts/install_python.ps1 3.6.8 Python36 $env:CPU_ARCH
 if ($LastExitCode -ne 0) { exit 1; }
 
