21b6dce1549237a3c53ec3fcd162d753995d9165
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-05-07 17:57:26 +0200

    Implement version sourcing from SCM
    This change integrates setuptools-scm for deriving the dist
    version from Git tags

--- src/pylibsshext/__init__.py
+++ src/pylibsshext/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Python bindings for libssh."""
+
+from ._version import (  # noqa: F401, WPS300, WPS436
+    __full_version__, __libssh_version__, __version__, __version_info__,
+)
