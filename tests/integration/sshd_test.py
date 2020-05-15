0ae304f783fa1f38bee5034361bb00d7b9b567b7
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-05-16 01:09:47 +0200

    🚑 Don't lint test_sshd_addr_fixture_port args

--- tests/integration/sshd_test.py
+++ tests/integration/sshd_test.py
@@ -6,6 +6,9 @@ MAX_PORT_NUMBER = 65535
 
 
 def test_sshd_addr_fixture_port(sshd_addr):
-    """Smoke-test sshd_addr fixture."""
+    """Smoke-test sshd_addr fixture.
+
+    # noqa: DAR101
+    """
     _host, port = sshd_addr
     assert 0 < port <= MAX_PORT_NUMBER
