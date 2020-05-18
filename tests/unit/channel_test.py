2495ac3d415b60a025a2eea699365a57852f931d
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-05-18 17:53:34 +0200

    Add some trivial ssh_client_session channel tests

--- tests/unit/channel_test.py
+++ tests/unit/channel_test.py
@@ -4,14 +4,37 @@
 
 import pytest
 
-from pylibsshext.channel import Channel
-from pylibsshext.session import Session
+
+@pytest.fixture
+def ssh_channel(ssh_client_session):
+    """Initialize a channel and tear it down after testing.
+
+    :param ssh_client_session: A pre-authenticated SSH session.
+    :type ssh_client_session: pylibsshext.session.Session
+
+    :yields: A libssh channel instance.
+    :ytype: pylibsshext.channel.Channel
+    """
+    chan = ssh_client_session.new_channel()
+    try:  # noqa: WPS501
+        yield chan
+    finally:
+        chan.close()
 
 
-@pytest.mark.xfail(
-    reason='Fails for an unknown reason',
-    raises=MemoryError,
-)
-def test_make_channel():
-    """Smoke-test Channel instance creation."""
-    assert Channel(Session())
+def test_exec_command(ssh_channel):
+    """Test getting the output of a remotely executed command."""
+    u_cmd_out = ssh_channel.exec_command('echo -n Hello World').stdout.decode()
+    assert u_cmd_out == u'Hello World'  # noqa: WPS302
+
+
+def test_double_close(ssh_channel):
+    """Test that closing the channel multiple times doesn't explode."""
+    for _ in range(3):
+        ssh_channel.close()
+
+
+def test_channel_exit_status(ssh_channel):
+    """Test retrieving a channel exit status upon close."""
+    ssh_channel.close()
+    assert ssh_channel.get_channel_exit_status() == -1
