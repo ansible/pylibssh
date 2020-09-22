25816f269f174ff89bc300d5a692c8be5d648fc2
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-09-22 09:47:22 -0500

    bump openssl to 1.1.1h (#299)
    * bump openssl to 1.1.1h
    
    * fix for libenchant
    
    * make updates consistent

--- cryptography-manylinux/openssl-version.sh
+++ cryptography-manylinux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-1.1.1g"
-export OPENSSL_SHA256="ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46"
+export OPENSSL_VERSION="openssl-1.1.1h"
+export OPENSSL_SHA256="5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
