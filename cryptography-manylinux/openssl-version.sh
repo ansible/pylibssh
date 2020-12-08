5e83d2bd47d42685deebe5ce00ccdc00ab069b9b
Alex Gaynor <alex.gaynor@gmail.com>
2020-12-08 10:42:45 -0500

    OpenSSL 1.1.1i (#317)

--- cryptography-manylinux/openssl-version.sh
+++ cryptography-manylinux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-1.1.1h"
-export OPENSSL_SHA256="5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9"
+export OPENSSL_VERSION="openssl-1.1.1i"
+export OPENSSL_SHA256="e8be6a35fe41d10603c3cc635e93289ed00bf34b79671a3a4de64fcee00d5242"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
