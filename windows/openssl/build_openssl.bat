147cd4e521a2cfac822d7902471de9408958cd31
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-02-21 16:17:49 -0600

    stop building VS2010 OpenSSL (#331)
    * stop building VS2010 OpenSSL
    
    we don't need it now that we've dropped py27
    
    * simplify more
    
    * don't need this powershell script since we're not installing vs2010 now

--- windows/openssl/build_openssl.bat
+++ windows/openssl/build_openssl.bat
@@ -1,19 +1,10 @@
 SET BUILDARCH=%1
-SET VSVERSION=%2
 
 cd openssl-1*
 
-if "%VSVERSION%" == "2010" (
-    if "%BUILDARCH%" == "win32" (
-        CALL ..\windows\openssl\build_openssl_win32_2010.bat
-    ) else (
-        CALL ..\windows\openssl\build_openssl_win64_2010.bat
-    )
+if "%BUILDARCH%" == "win32" (
+    CALL ..\windows\openssl\build_openssl_win32_2019.bat
 ) else (
-    if "%BUILDARCH%" == "win32" (
-        CALL ..\windows\openssl\build_openssl_win32_2019.bat
-    ) else (
-        CALL ..\windows\openssl\build_openssl_win64_2019.bat
-    )
+    CALL ..\windows\openssl\build_openssl_win64_2019.bat
 )
 if %errorlevel% neq 0 exit /b %errorlevel%
