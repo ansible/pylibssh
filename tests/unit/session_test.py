1641a70781e1defe2a2a28f1351eb1c56cd9af95
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-03-30 22:56:19 +0200

    Add a test for session connection refused

--- tests/unit/session_test.py
+++ tests/unit/session_test.py
@@ -2,9 +2,20 @@
 
 """Tests suite for session."""
 
+import pytest
+
+from pylibsshext.errors import LibsshSessionException
 from pylibsshext.session import Session
 
 
 def test_make_session():
     """Smoke-test Session instance creation."""
     assert Session()
+
+
+def test_session_connection_refused(free_port_num):
+    """Test that connecting to a missing service raises an error."""
+    error_msg = r'^ssh connect failed: Connection refused$'
+    ssh_session = Session()
+    with pytest.raises(LibsshSessionException, match=error_msg):
+        ssh_session.connect(host='127.0.0.1', port=free_port_num)
