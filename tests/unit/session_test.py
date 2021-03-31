870f65fceb08dc0e76c1d51af4ff7a09533ef761
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-03-31 17:24:37 +0200

    Fix WPS360 linting violation in `session_test`

--- tests/unit/session_test.py
+++ tests/unit/session_test.py
@@ -15,7 +15,7 @@ def test_make_session():
 
 def test_session_connection_refused(free_port_num):
     """Test that connecting to a missing service raises an error."""
-    error_msg = r'^ssh connect failed: Connection refused$'
+    error_msg = '^ssh connect failed: Connection refused$'
     ssh_session = Session()
     with pytest.raises(LibsshSessionException, match=error_msg):
         ssh_session.connect(host='127.0.0.1', port=free_port_num)
