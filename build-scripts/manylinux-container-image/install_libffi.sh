faf3f9ebe158dc26b8f686b81d561b5aace2bcb6
Nathaniel Case <ncase@redhat.com>
2022-04-26 17:09:56 -0400

    Split out package updates
    Be more surgical installing things
    
    And borrow fetch_source as it seems useful

--- build-scripts/manylinux-container-image/install_libffi.sh
+++ build-scripts/manylinux-container-image/install_libffi.sh
@@ -3,23 +3,21 @@ set -xe
 
 unset RELEASE
 
+# Get script directory
+MY_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Get build utilities
+source $MY_DIR/build_utils.sh
+
 LIBFFI_SHA256="72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056"
 LIBFFI_VERSION="3.3"
 
-function check_sha256sum {
-    local fname=$1
-    local sha256=$2
-    echo "${sha256}  ${fname}" > "${fname}.sha256"
-    sha256sum -c "${fname}.sha256"
-    rm "${fname}.sha256"
-}
-
-curl -#O "https://mirrors.ocf.berkeley.edu/debian/pool/main/libf/libffi/libffi_${LIBFFI_VERSION}.orig.tar.gz"
+fetch_source libffi_${LIBFFI_VERSION}.orig.tar.gz https://mirrors.ocf.berkeley.edu/debian/pool/main/libf/libffi
 check_sha256sum "libffi_${LIBFFI_VERSION}.orig.tar.gz" ${LIBFFI_SHA256}
-tar zxf libffi*.orig.tar.gz
-PATH=/opt/perl/bin:$PATH
+tar zxf libffi_${LIBFFI_VERSION}.orig.tar.gz
 pushd libffi*/
 if [[ "$1" =~ '^manylinux1_.*$' ]]; then
+  PATH=/opt/perl/bin:$PATH
   STACK_PROTECTOR_FLAGS="-fstack-protector --param=ssp-buffer-size=4"
 else
   STACK_PROTECTOR_FLAGS="-fstack-protector-strong"
--- build-scripts/manylinux-container-image/install_openssl.sh
+++ build-scripts/manylinux-container-image/install_openssl.sh
@@ -3,22 +3,22 @@ set -xe
 
 unset RELEASE
 
+# Get script directory
+MY_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Get build utilities
+source $MY_DIR/build_utils.sh
+
 OPENSSL_URL="https://www.openssl.org/source/"
 source /root/openssl-version.sh
 
-function check_sha256sum {
-    local fname=$1
-    local sha256=$2
-    echo "${sha256}  ${fname}" > "${fname}.sha256"
-    sha256sum -c "${fname}.sha256"
-    rm "${fname}.sha256"
-}
-
-curl -#O "${OPENSSL_URL}/${OPENSSL_VERSION}.tar.gz"
-check_sha256sum ${OPENSSL_VERSION}.tar.gz ${OPENSSL_SHA256}
+fetch_source ${OPENSSL_VERSION}.tar.gz ${OPENSSL_URL}
+check_sha256sum "${OPENSSL_VERSION}.tar.gz" ${OPENSSL_SHA256}
 tar zxf ${OPENSSL_VERSION}.tar.gz
-PATH=/opt/perl/bin:$PATH
 pushd ${OPENSSL_VERSION}
+if [[ "$1" =~ '^manylinux1_.*$' ]]; then
+  PATH=/opt/perl/bin:$PATH
+fi
 ./config $OPENSSL_BUILD_FLAGS --prefix=/opt/pyca/cryptography/openssl --openssldir=/opt/pyca/cryptography/openssl
 make depend
 make -j4
