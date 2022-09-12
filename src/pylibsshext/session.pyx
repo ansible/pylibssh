2530dd073648bbe4a11c495130ffe178a798dd63
Steven A Broderick Elias <sabedevops@gmail.com>
2022-09-12 09:31:38 -0400

    Support specifying socket fd for connection. (#343)
    * Support specifying socket fd for connection.
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kate Case <this.is@katherineca.se>

--- src/pylibsshext/session.pyx
+++ src/pylibsshext/session.pyx
@@ -26,6 +26,7 @@ from pylibsshext.sftp import SFTP
 
 
 OPTS_MAP = {
+    "fd": libssh.SSH_OPTIONS_FD,
     "host": libssh.SSH_OPTIONS_HOST,
     "user": libssh.SSH_OPTIONS_USER,
     "port": libssh.SSH_OPTIONS_PORT,
@@ -158,7 +159,7 @@ cdef class Session(object):
             key_m = OPTS_MAP[key]
         else:
             raise LibsshSessionException("Unknown attribute name [%s]" % key)
-        if key == "gssapi_delegate_credentials":
+        if key in ("fd", "gssapi_delegate_credentials"):
             value_int = value
             libssh.ssh_options_set(self._libssh_session, key_m, &value_int)
         elif key == "port":
@@ -178,6 +179,9 @@ cdef class Session(object):
         """Conenct to ssh server and negotiate libssh session by
         optionally verifying the server's host key and authenticate
         either by password or private key.
+
+        :param fd: The file descriptor of the socket to use for the connection.
+        :type fd: int
 
         :param host: The address of the remote host
         :type host: str
