6d11d9cc8d2f62736f902af3de419d575475e49a
Kate Case <kcase@redhat.com>
2023-10-27 13:28:00 -0400

    Support changing SSH_OPTIONS_PUBLICKEY_ACCEPTED_TYPES

--- src/pylibsshext/session.pyx
+++ src/pylibsshext/session.pyx
@@ -33,6 +33,7 @@ OPTS_MAP = {
     "timeout": libssh.SSH_OPTIONS_TIMEOUT,
     "knownhosts": libssh.SSH_OPTIONS_KNOWNHOSTS,
     "proxycommand": libssh.SSH_OPTIONS_PROXYCOMMAND,
+    "publickey_accepted_types": libssh.SSH_OPTIONS_PUBLICKEY_ACCEPTED_TYPES,
     "gssapi_server_identity": libssh.SSH_OPTIONS_GSSAPI_SERVER_IDENTITY,
     "gssapi_client_identity": libssh.SSH_OPTIONS_GSSAPI_CLIENT_IDENTITY,
     "gssapi_delegate_credentials": libssh.SSH_OPTIONS_GSSAPI_DELEGATE_CREDENTIALS,
