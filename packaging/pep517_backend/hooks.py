35733840e176fbc97adb88a0d5906474d127fd75
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-11-29 02:37:11 +0100

    📦🐛 Fix PEP 660 editable install mode
    Previously, due to restrictive PEP 517 hook reimports,
    our in-tree build backend was losing non-PEP 517 hooks
    implemented in newer versions of `setuptools` but not
    the earlier ones. This is now addressed by reexporting
    everything that `setuptools` exposes with a wildcard.

--- packaging/pep517_backend/hooks.py
+++ packaging/pep517_backend/hooks.py
@@ -2,13 +2,18 @@
 
 """PEP 517 build backend pre-building Cython exts before setuptools."""
 
-from ._backend import (  # noqa: WPS436  # Re-exporting PEP 517 hooks
-    build_sdist, build_wheel, get_requires_for_build_sdist,
-    get_requires_for_build_wheel, prepare_metadata_for_build_wheel,
-)
+from contextlib import suppress as _suppress
+
+# Re-exporting PEP 517 hooks
+# pylint: disable-next=unused-wildcard-import,wildcard-import
+from setuptools.build_meta import *  # noqa: E501, F403, WPS347
+
+# Re-exporting PEP 517 hooks
+from ._backend import build_wheel  # noqa: WPS436
 
 
-__all__ = (  # noqa: WPS317, WPS410
-    'build_sdist', 'build_wheel', 'get_requires_for_build_sdist',
-    'get_requires_for_build_wheel', 'prepare_metadata_for_build_wheel',
-)
+with _suppress(ImportError):  # Only succeeds w/ setuptools implementing PEP 660
+    # Re-exporting PEP 660 hooks
+    from ._backend import (  # type: ignore[assignment]  # noqa: WPS433, WPS436
+        build_editable,
+    )
