e369e7d785fecae0be19b8238a53c075c909efc6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-01-27 01:32:01 +0100

    📦🎨 Convert Cython helper to context manager

--- packaging/pep517_backend/_backend.py
+++ packaging/pep517_backend/_backend.py
@@ -5,12 +5,12 @@
 from __future__ import annotations
 
 import os
-from contextlib import contextmanager, suppress
-from functools import wraps
+import typing as t  # noqa: WPS111
+from contextlib import contextmanager, nullcontext, suppress
 from sys import version_info as _python_version_tuple
 
 from setuptools.build_meta import (  # noqa: F401  # Re-exporting PEP 517 hooks
-    build_wheel,
+    build_wheel as _setuptools_build_wheel,
 )
 from setuptools.build_meta import (
     get_requires_for_build_wheel as _setuptools_get_requires_for_build_wheel,
@@ -93,18 +93,27 @@ def patched_dist_has_ext_modules():
         DistutilsDistribution.has_ext_modules = orig_func
 
 
-def pre_build_cython(orig_func):  # noqa: WPS210
-    """Pre-build cython exts before invoking ``orig_func``.
+@contextmanager
+def _prebuild_c_extensions(
+        line_trace_cython_when_unset: bool = False,  # noqa: WPS318
+        build_inplace: bool = False,
+        config_settings: dict[str, str] | None = None,
+) -> t.Generator[None, t.Any, t.Any]:
+    """Pre-build C-extensions in a temporary directory, when needed.
 
-    :param orig_func: function being wrapped
-    :type orig_func: callable
+    This context manager also patches metadata, setuptools and distutils.
 
-    :returns: function wrapper
-    :rtype: callable
+    :param build_inplace: Whether to copy and chdir to a temporary location.
+    :param config_settings: :pep:`517` config settings mapping.
+
     """
-    cython_line_tracing_requested = os.getenv('ANSIBLE_PYLIBSSH_TRACING') == '1'
-    @wraps(orig_func)
-    def func_wrapper(*args, **kwargs):  # noqa: WPS210, WPS430
+    cython_line_tracing_requested = os.getenv(
+        'ANSIBLE_PYLIBSSH_TRACING',
+        line_trace_cython_when_unset,
+    ) == '1'
+
+    build_dir_ctx = nullcontext()
+    with build_dir_ctx:
         config = _get_local_cython_config()
 
         cythonize_args = _make_cythonize_cli_args_from_config(config)
@@ -112,8 +121,34 @@ def pre_build_cython(orig_func):  # noqa: WPS210
             _cythonize_cli_cmd(cythonize_args)
         with patched_distutils_cmd_install():
             with patched_dist_has_ext_modules():
-                return orig_func(**kwargs)
-    return func_wrapper
+                yield
+
+
+@convert_to_kwargs_only
+def build_wheel(
+        wheel_directory: str,  # noqa: WPS318
+        config_settings: dict[str, str] | None = None,
+        metadata_directory: str | None = None,
+) -> str:
+    """Produce a built wheel.
+
+    This wraps the corresponding ``setuptools``' build backend hook.
+
+    :param wheel_directory: Directory to put the resulting wheel in.
+    :param config_settings: :pep:`517` config settings mapping.
+    :param metadata_directory: :file:`.dist-info` directory path.
+
+    """
+    with _prebuild_c_extensions(
+            line_trace_cython_when_unset=False,  # noqa: WPS318
+            build_inplace=False,
+            config_settings=config_settings,
+    ):
+        return _setuptools_build_wheel(
+            wheel_directory=wheel_directory,
+            config_settings=config_settings,
+            metadata_directory=metadata_directory,
+        )
 
 
 def get_requires_for_build_wheel(
@@ -133,10 +168,6 @@ def get_requires_for_build_wheel(
         config_settings=config_settings,
     ) + c_ext_build_deps
 
-
-build_wheel = convert_to_kwargs_only(  # pylint: disable=invalid-name
-    pre_build_cython(build_wheel),
-)
 
 if _setuptools_build_editable is not None:
     build_editable = build_wheel
