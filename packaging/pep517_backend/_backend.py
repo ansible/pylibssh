a129b57c1f4051c603415eea3aa46b14a906b787
KB-perByte <paul.sagar@yahoo.com>
2025-06-23 15:14:40 +0530

    revert and add 3.13 to non mac

--- packaging/pep517_backend/_backend.py
+++ packaging/pep517_backend/_backend.py
@@ -328,7 +328,7 @@ def get_requires_for_build_wheel(
     """
     c_ext_build_deps = [
         'Cython >= 3.0.11; python_version >= "3.13"',  # Ideally >= 3.1.0
-        #'Cython >= 3.0.0; python_version >= "3.12" and python_version < "3.13"',
+        'Cython >= 3.0.0; python_version >= "3.12" and python_version < "3.13"',
         'Cython; python_version < "3.12"',
     ]
 
