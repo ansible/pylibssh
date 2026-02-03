285becf1f5098345971e80f17913acf1890f3d0c
komaldesai13 <kodesai@redhat.com>
2026-02-03 19:19:57 +0530

    added script to generate universal2 macos artifacst

---
+++ packaging/macos/build-libssh-macos.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -e
+
+# Prompt for build parameters
+read -p "libssh version [0.10.6]: " LIBSSH_VERSION
+LIBSSH_VERSION=${LIBSSH_VERSION:-0.10.6}
+
+read -p "OpenSSL version [3.0.13]: " OPENSSL_VERSION
+OPENSSL_VERSION=${OPENSSL_VERSION:-3.0.13}
+
+read -p "Architecture (arm64/x86_64) [$(uname -m)]: " ARCH
+ARCH=${ARCH:-$(uname -m)}
+
+OUTPUT_DIR="artifacts/${ARCH}"
+mkdir -p ${OUTPUT_DIR}
+
+WORK_DIR=$(mktemp -d)
+cd ${WORK_DIR}
+
+echo "Building in ${WORK_DIR}"
+echo "libssh: ${LIBSSH_VERSION}, OpenSSL: ${OPENSSL_VERSION}, Arch: ${ARCH}"
+
+export MACOSX_DEPLOYMENT_TARGET="10.13"
+
+# Build OpenSSL
+echo "Downloading OpenSSL..."
+curl -sL https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz | tar xz
+cd openssl-${OPENSSL_VERSION}
+
+if [ "${ARCH}" = "arm64" ]; then
+    OPENSSL_TARGET="darwin64-arm64-cc"
+else
+    OPENSSL_TARGET="darwin64-x86_64-cc"
+fi
+
+./Configure ${OPENSSL_TARGET} --prefix=${WORK_DIR}/deps no-shared no-tests
+make -j$(sysctl -n hw.ncpu) > /dev/null
+make install_sw > /dev/null
+cd ..
+
+# Build libssh
+echo "Downloading libssh..."
+curl -sL https://www.libssh.org/files/$(echo ${LIBSSH_VERSION} | cut -d. -f1-2)/libssh-${LIBSSH_VERSION}.tar.xz | tar xJ
+cd libssh-${LIBSSH_VERSION}
+mkdir build && cd build
+
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${WORK_DIR}/deps \
+    -DCMAKE_OSX_ARCHITECTURES=${ARCH} \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+    -DOPENSSL_ROOT_DIR=${WORK_DIR}/deps \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DWITH_EXAMPLES=OFF \
+    -DWITH_SERVER=OFF \
+    -DUNIT_TESTING=OFF \
+    > /dev/null
+
+make -j$(sysctl -n hw.ncpu) > /dev/null
+make install > /dev/null
+
+# Package artifacts
+cd ${WORK_DIR}/deps
+ARTIFACT_NAME="libssh-${LIBSSH_VERSION}-openssl-${OPENSSL_VERSION}-macos-${ARCH}"
+mkdir -p ${ARTIFACT_NAME}/{lib,include}
+
+cp lib/*.a ${ARTIFACT_NAME}/lib/
+cp -r include/* ${ARTIFACT_NAME}/include/
+
+tar czf ${ARTIFACT_NAME}.tar.gz ${ARTIFACT_NAME}
+shasum -a 256 ${ARTIFACT_NAME}.tar.gz > ${ARTIFACT_NAME}.tar.gz.sha256
+
+# Move to output directory
+mv ${ARTIFACT_NAME}.tar.gz* ${OLDPWD}/${OUTPUT_DIR}/
+
+cd ${OLDPWD}
+rm -rf ${WORK_DIR}
+
+echo "Done. Artifacts in ${OUTPUT_DIR}/"
+ls -lh ${OUTPUT_DIR}/
---
+++ packaging/macos/merge-universal2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <arm64_dir> <x86_64_dir> <output_dir>"
+    exit 1
+fi
+
+ARM64_DIR=$1
+X86_64_DIR=$2
+OUTPUT_DIR=$3
+
+if [ ! -d "${ARM64_DIR}" ] || [ ! -d "${X86_64_DIR}" ]; then
+    echo "Error: Input directories don't exist"
+    exit 1
+fi
+
+mkdir -p ${OUTPUT_DIR}/{lib,include}
+
+echo "Merging ${ARM64_DIR} and ${X86_64_DIR} into ${OUTPUT_DIR}"
+
+# Copy headers from arm64 (should be identical)
+cp -r ${ARM64_DIR}/include/* ${OUTPUT_DIR}/include/
+
+# Merge libraries using lipo
+for lib in ${ARM64_DIR}/lib/*.a; do
+    libname=$(basename ${lib})
+    if [ -f "${X86_64_DIR}/lib/${libname}" ]; then
+        echo "Merging ${libname}"
+        lipo -create ${ARM64_DIR}/lib/${libname} ${X86_64_DIR}/lib/${libname} \
+            -output ${OUTPUT_DIR}/lib/${libname}
+    fi
+done
+
+echo "Done. Universal2 artifacts in ${OUTPUT_DIR}/"
+file ${OUTPUT_DIR}/lib/*.a | head -3
