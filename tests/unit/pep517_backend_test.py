82d1f0502ebecaa227e316a41204da49d6f75bcc
J. Nick Koston <nick@home-assistant.io>
2026-09-15 19:55:07 -0500

    Name the flag variables once in the backend test for the style checker

--- tests/unit/pep517_backend_test.py
+++ tests/unit/pep517_backend_test.py
@@ -10,6 +10,8 @@ import pytest
 from pep517_backend._cython_configuration import patched_env
 
 
+CFLAGS_VAR = 'CFLAGS'
+CPPFLAGS_VAR = 'CPPFLAGS'
 TRACE_MACRO = '-DCYTHON_TRACE_NOGIL=1'
 
 
@@ -21,16 +23,16 @@ def test_tracing_macro_goes_through_cppflags(monkeypatch, tracing):
     flags in setuptools' distutils, which silently drops -O3 from the
     build, while CPPFLAGS is appended to them.
     """
-    monkeypatch.setenv('CFLAGS', '-fuser-cflag')
-    monkeypatch.setenv('CPPFLAGS', '-DUSER=1')
+    monkeypatch.setenv(CFLAGS_VAR, '-fuser-cflag')
+    monkeypatch.setenv(CPPFLAGS_VAR, '-DUSER=1')
 
     with patched_env({}, cython_line_tracing_requested=tracing):
-        cppflags = shlex.split(os.environ['CPPFLAGS'])
-        assert os.environ['CFLAGS'] == '-fuser-cflag'
+        cppflags = shlex.split(os.environ[CPPFLAGS_VAR])
+        assert os.environ[CFLAGS_VAR] == '-fuser-cflag'
 
     assert cppflags[0] == '-DUSER=1'
     assert (TRACE_MACRO in cppflags) is tracing
-    assert os.environ['CPPFLAGS'] == '-DUSER=1'
+    assert os.environ[CPPFLAGS_VAR] == '-DUSER=1'
 
 
 @pytest.mark.skipif(
@@ -44,14 +46,14 @@ def test_interpreter_flags_survive_the_build_env(monkeypatch):
     backend ever goes back to setting CFLAGS.
     """
     # A developer shell exporting these would replace the flags up front.
-    monkeypatch.delenv('CFLAGS', raising=False)
-    monkeypatch.delenv('CPPFLAGS', raising=False)
+    monkeypatch.delenv(CFLAGS_VAR, raising=False)
+    monkeypatch.delenv(CPPFLAGS_VAR, raising=False)
     # The RPM builds run the suite without setuptools available.
     ccompiler = pytest.importorskip('setuptools._distutils.ccompiler')
     distutils_sysconfig = pytest.importorskip(
         'setuptools._distutils.sysconfig',
     )
-    interpreter_flags = shlex.split(sysconfig.get_config_var('CFLAGS') or '')
+    interpreter_flags = shlex.split(sysconfig.get_config_var(CFLAGS_VAR) or '')
 
     with patched_env({}, cython_line_tracing_requested=True):
         compiler = ccompiler.new_compiler()
